0d40e95ef322149663097f3582186283a5941593
senku-sen <fannyzilong91@gmail.com>
2025-10-21 23:29:57 +0800

    Update change in Header
    Header text update for title and sub title

--- vaxsync/app/inventory/page.js
+++ vaxsync/app/inventory/page.js
@@ -1,12 +1,23 @@
 import Sidebar from "../../components/Sidebar";
+import Header from "../../components/Header";
 
-export default function Home() {
+export default function Inventory({
+  title = "Inventory Management",
+  subtitle = "Manage vaccine stock and supplies",
+  children,
+}) {
   return (
-    <div className="flex">
+    <div className="flex h-screen flex-col lg:flex-row">
+      {/* Sidebar */}
       <Sidebar />
-      <main className="p-6">
-        <h1>Main Content</h1>
-      </main>
+
+      <div className="flex-1 flex flex-col w-full lg:ml-64">
+        {/* Header */}
+        <Header title={title} subtitle={subtitle} />
+
+        {/* Main Content */}
+        <main className="p-4  flex-1 overflow-auto">{children}</main>
+      </div>
     </div>
   );
 }
