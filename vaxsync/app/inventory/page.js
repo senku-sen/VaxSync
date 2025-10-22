96926c652849d9d3d98e37c2e037c698533b0d4d
senku-sen <fannyzilong91@gmail.com>
2025-10-22 22:29:17 +0800

    changes

--- vaxsync/app/inventory/page.js
+++ vaxsync/app/inventory/page.js
@@ -1,9 +1,14 @@
 import Sidebar from "../../components/Sidebar";
-
+import Header from "../../components/Header";
 export default function Home() {
   return (
     <div className="flex">
       <Sidebar />
+      <Header
+        title="Inventory"
+        subtitle="Manage your vaccine inventory here."
+      />
+
       <main className="p-6">
         <h1>Main Content</h1>
       </main>
