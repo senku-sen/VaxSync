1c598dd8d8353970b0b91320c0824f16e023369f
senku-sen <fannyzilong91@gmail.com>
2025-10-25 03:35:25 +0800

    relocation

--- vaxsync/app/Head_Nurse/inventory/page.js
+++ vaxsync/app/Head_Nurse/inventory/page.js
@@ -1,7 +1,7 @@
 "use client";
 
-import Sidebar from "../../components/Sidebar";
-import Header from "../../components/Header";
+import Sidebar from "../../../components/Sidebar";
+import Header from "../../../components/Header";
 import AddVaccine from "@/components/AddVaccine";
 import DeleteConfirm from "@/components/DeleteConfirm";
 import { Input } from "@/components/ui/input";
