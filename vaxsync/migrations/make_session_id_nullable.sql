b3ef288883b9eacb0f64fac997206dfa0aacc750
senku-sen <fannyzilong91@gmail.com>
2025-12-09 13:29:57 +0800

    removing all md files and sql files

--- vaxsync/migrations/make_session_id_nullable.sql
+++
@@ -1,8 +0,0 @@
--- Make session_id nullable in session_beneficiaries table
--- This allows storing custom vaccines/dates that are not associated with a specific session
-
-ALTER TABLE session_beneficiaries
-ALTER COLUMN session_id DROP NOT NULL;
-
--- Add comment explaining the change
-COMMENT ON COLUMN session_beneficiaries.session_id IS 'Foreign key to vaccination_sessions. Can be NULL for custom vaccines/dates not tied to a specific session.';
